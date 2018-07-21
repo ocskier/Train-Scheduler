a0eb2c828a5bd6264954d744af82051169660f15
Pack Fan 1031 <ocskier@gmail.com>
2018-07-20 23:29:32 -0400

    almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ var config = {
     messagingSenderId: "277978229879"
   };
   
-      firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 var db = firebase.database();
 var jjdb = db.ref("TrainScheduler");
@@ -32,23 +32,40 @@ connectedRef.on("value", function(snap) {
   }
 });
 
-jjdb.on("value", function(snapshot) {
+var giphyGifPos=0;
+function queryGiphy (cat) {
 
-   
-  
-  // If any errors are experienced, log them to console.
-  }, function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  });
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    cat + "&api_key=dc6zaTOxFJmzC&limit=1&offset=" + giphyGifPos;
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+        console.log(response);
+        // write all the gifs that were returned 
+        for (i=0;i<response.data.length;i++){
+            // increment the spot in the database list 
+            giphyGifPos++;
+            // Increment the total search length by the number returned
+            // Write a new Gif card to window with the gif checkbox and fav buttons
+            var newGifDiv = $('<div class="card gif-card" style="width:100%;">');
+            newGifDiv.append('<img src="'+response.data[0].images.original.url+'" frameBorder="0" class = "card-img-top my-img" data-animate="'+response.data[0].images.original.url+'" data-still="'+response.data[0].images.original_still.url+'" data-state="animate" allowFullScreen></iframe>');
+            $("#gifrow").append(newGifDiv);
+        }
+    });            
+}
+queryGiphy("train");
 
   var name = "";
   var dest = "";
   var time = 0;
   var rawfreq = "";
 
-  function makeTrain (ref) {
-    ref.push({
+  var numTrains = 0;
+
+  function makeTrain () {
+    jjdb.push({
         Train: name,
         Destination: dest,
         StartTime: time,
@@ -68,67 +85,80 @@ jjdb.on("value", function(snapshot) {
     dest = $("#dest-input").val().trim();
     time = $("#time-input").val();
     console.log(time);
-    var randomFormat = "HH:mm";
-    var convertedTime = moment(time,randomFormat);
-    console.log(convertedTime);
     rawfreq = $("#min-input").val().trim();
 
     // Code for the push
-    makeTrain (jjdb);
+    makeTrain();
+    readFromDb();
+    $('input').val("");
   });
 
-  jjdb.on("child_added", function(childSnap) {
-      
-    // Log everything that's coming out of snapshot
-    console.log(childSnap.val().Train);
-    console.log(childSnap.val().Destination);
-    console.log(childSnap.val().StartTime);
-    console.log(childSnap.val().Freq);
-    console.log(childSnap.val().dateAdded);
-    
-    var momStartTime = moment(childSnap.val().StartTime,"HH:mm");
-    var totalMinAway = moment(moment()).diff(momStartTime, "minutes");
-    console.log(momStartTime);
-    var curTime = totalMinAway;
-    console.log(curTime);
+  function consoleTrain (child){
+    console.log(child.val().Train);
+    console.log(child.val().Destination);
+    console.log(child.val().StartTime);
+    console.log(child.val().Freq);
+    console.log(child.val().dateAdded);
+  }
 
-    var firstTime = moment(momStartTime).diff(moment("00:00","HH:mm"),"minutes");
+  function printTrain (child,next,min) {
+    var tr = $("<tr>"+numTrains+"></tr>");
+    tr.append("<td>"+child.val().Train+"</td>");
+    tr.append("<td>"+child.val().Destination+"</td>");
+    tr.append("<td>"+child.val().StartTime+"</td>");
+    tr.append("<td>"+child.val().Freq+"</td>");
+    tr.append('<td id="next-train-'+numTrains+'">'+next+"</td>");
+    tr.append('<td id="mins-'+numTrains+'">'+min+"</td>");
+    $("tbody").append(tr);
+  }
+
+  function calcNextTrain (childSnap) {
+      
+    var totalMinAway = moment(moment()).diff(moment(childSnap.val().StartTime,"HH:mm"), "minutes");
+
+    var firstTime = moment(moment(childSnap.val().StartTime,"HH:mm")).diff(moment("00:00","HH:mm"),"minutes");
     
     var lastTime = 0;
-    console.log(lastTime);
-    while (lastTime<curTime){
+
+    while (lastTime<totalMinAway){
         lastTime = lastTime + parseInt(childSnap.val().Freq);
-        console.log(lastTime);
     }
 
     var nextTime = lastTime + firstTime;
-    console.log(nextTime);
-   
+    
     var nextTimeMom = moment("00:00","HH:mm").add(nextTime,'minutes').format('HH:mm');
-    console.log(nextTimeMom);
 
     lastTime=(lastTime-parseInt(childSnap.val().Freq)) +firstTime;
-    console.log(lastTime);
 
     var h =  parseInt(lastTime / 60);
     var m = lastTime % 60;
     var lastTimeMom = h+":"+m;
-    console.log(lastTimeMom);
 
     var curMinAway =childSnap.val().Freq-moment(moment()).diff((moment(lastTimeMom,"HH:mm")),"minutes");
-    console.log(curMinAway);
 
     // full list of items to the well
-    var tr = $("<tr></tr>");
-    tr.append("<td>"+childSnap.val().Train+"</td>");
-    tr.append("<td>"+childSnap.val().Destination+"</td>");
-    tr.append("<td>"+childSnap.val().StartTime+"</td>");
-    tr.append("<td>"+childSnap.val().Freq+"</td>");
-    tr.append("<td>"+nextTimeMom+"</td>");
-    tr.append("<td>"+curMinAway+"</td>");
-    $("tbody").append(tr);
+    printTrain(childSnap,nextTimeMom,curMinAway);
+  }
 
+  var intervalId;
+
+  function readFromDb () {
+  jjdb.on("child_added", function(snapshot) {
+      
+    // Log everything that's coming out of snapshot
+        consoleTrain(snapshot);
+        calcNextTrain(snapshot);
+        setTimeout (jjdb.off(),1000);
+    
     // Handle the errors
-  }, function(errorObject) {
-    console.log("Errors handled: " + errorObject.code);
-  });
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
+  }
+
+  readFromDb();
+  
+  setInterval(function(){
+    $("tbody").empty();
+    readFromDb();
+  },1000*60);
